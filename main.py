af023a54bb500899977788b1309af019a1b477d0
Nik V <github@nikvdp.com>
2020-11-10 01:25:24 -0800

    🗡  add lark for parsing, first stab at grammar
    wip: grammar works, but not if there are multiple [[]]s

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import json
 from typing import List
+from lark import Lark
 
-from roam_types import from_roam_export, Page
+from roam_types import from_roam_export, Page, Block
+
+parser = Lark(open("./GRAMMAR").read())
 
 
 def load_roam_import(
@@ -11,4 +14,26 @@ def load_roam_import(
         return from_roam_export(json.load(f))
 
 
-print([x.string for x in load_roam_import()[0].children])
+def extract_children(thing):
+    res = []
+    if thing.children:
+        for child in thing.children:
+            res.append((child.uid, child))
+            if child.children:
+                res += extract_children(child)
+    return res
+
+
+def parse_block_text(block: Block):
+    return parser.parse(block.string)
+
+
+def build_flattened_blocklist(roam_pages: List[Page]):
+    blocks = {}
+    for page in roam_pages:
+        children = extract_children(page)
+        for block_tuple in children:
+            blocks[block_tuple[0]] = block_tuple[1]
+
+    return blocks
+blocks_by_id = build_flattened_blocklist([load_roam_import()[0]])
