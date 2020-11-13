172a4098edd670f1367a681136f725c70e62883e
Nik V <github@nikvdp.com>
2020-11-12 21:56:58 -0800

    🎉 woot! privacy controls now working properly

--- main.py
+++ main.py
@@ -13,25 +13,37 @@ INLINE_TAG_RE: re.Pattern = re.compile(r"#([A-Za-z0-9_-]+)")
 BACKLINK_TAG_RE: re.Pattern = re.compile(r"#\[\[([^\]]+)]\]")
 
 PUBLIC_PAGE_TAG = "public_page"
+PRIVATE_PAGE_TAG = "private_page"
 PUBLIC_TAG = "public"
 PRIVATE_TAG = "private"
 
 
 def load_roam_import(
-    roam_json_file="./nikvdp-public.json",
+    roam_json_file="./nikvdp-public-private.json",
 ) -> List[Page]:
     with open(roam_json_file) as f:
         return from_roam_export(json.load(f))
 
 
-def extract_children(thing):
+def extract_children(block_or_page):
     res = []
-    if thing.children:
-        for child in thing.children:
-            res.append((child.uid, child))
+    if block_or_page.children:
+        for child in block_or_page.children:
+            res.append(child)
             if child.children:
                 res += extract_children(child)
     return res
+
+
+def delete_child_blocks(root_block, block_id):
+    if not root_block.children or len(root_block.children) < 1:
+        return
+
+    for idx, block in enumerate(root_block.children):
+        if block.uid == block_id:
+            del root_block.children[idx]
+        if block.children and len(block.children) > 0:
+            delete_child_blocks(block, block_id)
 
 
 @dataclass
@@ -49,9 +61,9 @@ def build_flattened_blocklist(roam_pages: List[Page]):
     for page in roam_pages:
         pages[page.title] = page
         children = extract_children(page)
-        for block_id, block in children:
-            blocks[block_id] = block
-            block_to_page_map[block_id] = page
+        for block in children:
+            blocks[block.uid] = block
+            block_to_page_map[block.uid] = page
 
     return BlockMap(
         by_block_id=blocks, block_to_page=block_to_page_map, pages=pages
@@ -64,7 +76,7 @@ def collect_relevant_tags(block):
         for tag in (
             INLINE_TAG_RE.findall(block) + BACKLINK_TAG_RE.findall(block)
         )
-        if tag in [PUBLIC_TAG, PUBLIC_PAGE_TAG, PRIVATE_TAG]
+        if tag in [PUBLIC_TAG, PUBLIC_PAGE_TAG, PRIVATE_TAG, PRIVATE_PAGE_TAG]
     ]
 
 
@@ -101,35 +113,32 @@ for block_id, block in blocks_by_id.items():
                 flattened_import.block_to_page[block_id].title
             ].append(block)
         except Exception as e:
-            # set_trace()
             raise e
 
-
+# do a second pass to remove anything that was marked private
 for block_id, block in blocks_by_id.items():
     tags = collect_relevant_tags(block.string)
+    page = flattened_import.block_to_page[block_id]
+
+    if PRIVATE_PAGE_TAG in tags:
+        print(
+            f"Got #private_page tag on block {block_id} of page '{page.title}'"
+        )
+        if output_pages.get(page.title):
+            print(f"Deleting page {page.title}")
+            del output_pages[page.title]
 
     if PRIVATE_TAG in tags:
-        page = flattened_import.block_to_page[block_id]
+        print(f"Got #private tag on block {block_id} of page '{page.title}'")
+        output_pages[page.title] = [
+            b for b in output_pages[page.title] if b.uid != block_id
+        ]
 
-        def delete_child_blocks(root_block):
-            if not root_block.children or len(root_block.children) < 1:
-                return
-
-            for idx, block in enumerate(root_block.children):
-                # (block_id pulled in via closure here)
-                if block.uid == block_id:
-                    # print(f"Deleting block f{block}")
-                    del root_block.children[idx]
-
-                    # what i'm supposed to delete:
-                    #  del output_pages['roam2agora'][0].children[1]
-
-                    return
-                if block.children and len(block.children > 0):
-                    delete_child_blocks(block)
-
+        # recursively iterate through and make sure the privat block isn't
+        # nested
         for block in output_pages[page.title]:
-            delete_child_blocks(block)
+            delete_child_blocks(page, block_id)
+
 
 print("----\n")
 
