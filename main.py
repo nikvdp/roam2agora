53efab301a6ca5ea0ce17f0e224c634bdbe90d65
Nik V <github@nikvdp.com>
2020-11-13 16:48:26 -0800

    📎 add cli, markdown output

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
+#!/usr/bin/env python3
+import os
+import sys
 import json
 from typing import List
 from dataclasses import dataclass
 from collections import defaultdict
 import re
 
-from ipdb import set_trace
+try:
+    # TODO: for debugging only, delete soon
+    from ipdb import set_trace
+except:
+    pass
 from pprint import pprint
 
+import click
+
 from roam_types import from_roam_export, Page, Block
+
 
 INLINE_TAG_RE: re.Pattern = re.compile(r"#([A-Za-z0-9_-]+)")
 BACKLINK_TAG_RE: re.Pattern = re.compile(r"#\[\[([^\]]+)]\]")
@@ -18,9 +28,7 @@ PUBLIC_TAG = "public"
 PRIVATE_TAG = "private"
 
 
-def load_roam_import(
-    roam_json_file="./nikvdp-public-private.json",
-) -> List[Page]:
+def load_roam_import(roam_json_file) -> List[Page]:
     with open(roam_json_file) as f:
         return from_roam_export(json.load(f))
 
@@ -91,58 +99,102 @@ def format_block_recursive(block, indent=2, indent_step=4):
     return out
 
 
-roam_import = load_roam_import()
+def process_roam_export(roam_import: List[Page]) -> defaultdict:
+    flattened_import = build_flattened_blocklist(roam_import)
+    blocks_by_id = flattened_import.by_block_id
 
-flattened_import = build_flattened_blocklist(roam_import)
-blocks_by_id = flattened_import.by_block_id
+    output_pages = defaultdict(list)
 
-output_pages = defaultdict(list)
+    for block_id, block in blocks_by_id.items():
+        tags = collect_relevant_tags(block.string)
 
-for block_id, block in blocks_by_id.items():
-    tags = collect_relevant_tags(block.string)
-
-    if PUBLIC_PAGE_TAG in tags:
-        page = flattened_import.block_to_page[block_id]
-        output_pages[
-            flattened_import.block_to_page[block_id].title
-        ] += page.children
-
-    if PUBLIC_TAG in tags:
-        try:
+        if PUBLIC_PAGE_TAG in tags:
+            page = flattened_import.block_to_page[block_id]
             output_pages[
                 flattened_import.block_to_page[block_id].title
-            ].append(block)
-        except Exception as e:
-            raise e
+            ] += page.children
 
-# do a second pass to remove anything that was marked private
-for block_id, block in blocks_by_id.items():
-    tags = collect_relevant_tags(block.string)
-    page = flattened_import.block_to_page[block_id]
+        if PUBLIC_TAG in tags:
+            try:
+                output_pages[
+                    flattened_import.block_to_page[block_id].title
+                ].append(block)
+            except Exception as e:
+                raise e
 
-    if PRIVATE_PAGE_TAG in tags:
-        print(
-            f"Got #private_page tag on block {block_id} of page '{page.title}'"
+    # do a second pass to remove anything that was marked private
+    for block_id, block in blocks_by_id.items():
+        tags = collect_relevant_tags(block.string)
+        page = flattened_import.block_to_page[block_id]
+
+        if PRIVATE_PAGE_TAG in tags:
+            print(
+                f"Got #private_page tag on block {block_id} of page '{page.title}'"
+            )
+            if output_pages.get(page.title):
+                print(f"Deleting page {page.title}")
+                del output_pages[page.title]
+
+        if PRIVATE_TAG in tags:
+            print(
+                f"Got #private tag on block {block_id} of page '{page.title}'"
+            )
+            output_pages[page.title] = [
+                b for b in output_pages[page.title] if b.uid != block_id
+            ]
+
+            # recursively iterate through and make sure the private block isn't
+            # nested
+            for block in output_pages[page.title]:
+                delete_child_blocks(page, block_id)
+
+    return output_pages
+
+
+def sanitize_filename(filename) -> str:
+    # stolen from agora code here:
+    # https://github.com/flancian/agora-server/blob/39453d8b0758a6276face99fc7b78576e48136a0/app/db.py#L42
+    # TODO: probably should replace slashes too lest roam namespace pages screw up dir structure
+    return filename.lower().replace(" ", "-").replace("'", "").replace(",", "")
+
+
+@click.command(context_settings=dict(help_option_names=["-h", "--help"]))
+@click.option(
+    "--roam-export-file",
+    "-i",
+    type=click.Path(file_okay=True, dir_okay=False, exists=True),
+    prompt="Path to your roam export json file",
+)
+@click.option(
+    "--output-folder",
+    "-o",
+    default="./agora",
+    type=click.Path(file_okay=False, dir_okay=True),
+    prompt="Path to save garden files to",
+)
+def main(roam_export_file, output_folder="./agora"):
+    roam_import = load_roam_import(roam_export_file)
+
+    if not os.path.exists(output_folder):
+        os.mkdir(output_folder)
+
+    output_pages = process_roam_export(roam_import)
+
+    for page_title, blocks in output_pages.items():
+        filename = f"{sanitize_filename(page_title)}.md"
+
+        sys.stdout.write(
+            f"Writing page '{page_title}' to '{filename}'... ",
         )
-        if output_pages.get(page.title):
-            print(f"Deleting page {page.title}")
-            del output_pages[page.title]
 
-    if PRIVATE_TAG in tags:
-        print(f"Got #private tag on block {block_id} of page '{page.title}'")
-        output_pages[page.title] = [
-            b for b in output_pages[page.title] if b.uid != block_id
-        ]
+        with open(os.path.join(output_folder, filename), "w") as out_file:
+            out_file.write(f"# {page_title}\n\n")
 
-        # recursively iterate through and make sure the privat block isn't
-        # nested
-        for block in output_pages[page.title]:
-            delete_child_blocks(page, block_id)
+            for block in blocks:
+                out_file.write(format_block_recursive(block))
+
+        print(f"DONE!")
 
 
-print("----\n")
-
-for page_title, blocks in output_pages.items():
-    print(f"{page_title}:")
-    for block in blocks:
-        print(format_block_recursive(block))
+if __name__ == "__main__":
+    main()
